2e873117155c0a952532ddb9acfd1313efd5d2cc
Senay <senaialema@gmail.com>
2022-02-16 11:47:16 -0500

    ticketsales oop program

---
+++ ticketsales.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include "sportticket.h"
+
+using namespace std;
+
+class TicketSales {
+
+    public:
+
+        string print_ticket(ShowTicket *myticket){
+
+           return myticket->print_ticket();
+
+        }
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main () {
+  TicketSales ts;
+  ShowTicket myticket1("AA","101");
+  SportTicket myticket2("AA","102");
+  myticket1.sell_seat();
+  myticket2.sell_seat();
+  myticket2.sell_beer();
+  cout << ts.print_ticket(&myticket1);
+  cout << ts.print_ticket(&myticket2);
+  return 0;
+}
